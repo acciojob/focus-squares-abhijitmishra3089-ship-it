98c0fac4b94108f78800f4b5da610da7fe464349
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-05-11 11:32:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+const cla = document.getElementsByClassName("square");
+
+for (let i = 0; i < cla.length; i++) {
+
+    cla[i].addEventListener("mouseover", () => {
+
+        if (i === 0) {
+            cla[1].style.backgroundColor = "#6F4E37";
+            cla[2].style.backgroundColor = "#6F4E37";
+
+        } else if (i === 1) {
+            cla[0].style.backgroundColor = "#6F4E37";
+            cla[2].style.backgroundColor = "#6F4E37";
+
+        } else {
+            cla[0].style.backgroundColor = "#6F4E37";
+            cla[1].style.backgroundColor = "#6F4E37";
+        }
+
+    });
+cla[i].addEventListener("mouseout", () => {
+
+        if (i === 0) {
+            cla[1].style.backgroundColor = "#E6E6Fa";
+            cla[2].style.backgroundColor = "#E6E6Fa";
+
+        } else if (i === 1) {
+            cla[0].style.backgroundColor = "#E6E6Fa";
+            cla[2].style.backgroundColor = "#E6E6Fa";
+
+        } else {
+            cla[0].style.backgroundColor = "#E6E6Fa";
+            cla[1].style.backgroundColor = "#E6E6Fa";
+        }
+
+    });
+}
